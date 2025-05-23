74ca0512f921e7e1bed0d27cacf9e867adc3fd13
Bar Alon <baral4@ac.sce.ac.il>
2025-05-23 11:18:46 +0300

    BSPM25T16-258 Add student requests view in student page

--- frontend/src/components/Student.jsx
+++ frontend/src/components/Student.jsx
@@ -1,37 +1,41 @@
-//most recent
 import React, { useEffect, useState } from "react";
-import Header from '../header';
-import { getFromLocalStorage } from '../utils/services';
-import StudentRequestForm from '../components/StudentRequestForm';
-import './Welcome.css';
+import Header from "../header";
+import { getFromLocalStorage } from "../utils/services";
+import StudentRequestForm from "../components/StudentRequestForm";
+import "./Welcome.css";
+import StudentRequestsTable from "./StudentRequestsTable";
 
 const Student = () => {
-    const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState(null);
 
-    useEffect(() => {
-        const data = getFromLocalStorage('projectFS');
-        setUserData(data);
-    }, []);
+  useEffect(() => {
+    const data = getFromLocalStorage("projectFS");
+    setUserData(data);
+  }, []);
 
-    return (
-        <div className="welcome">
-            <div className="welcome-page-container">
-                <Header />
-                <div className="welcome-header-box">
-                    {userData && (
-                        <>
-                            <h2>ברוך הבא לאזור האישי שלך {userData.user.username}</h2>
-                        </>
-                    )}
-                </div>
-
-                {/* מציגים את הטופס כולל הכפתור שכבר יש בו */}
-                <div style={{ marginTop: '30px' }}>
-                    <StudentRequestForm />
-                </div>
-            </div>
+  return (
+    <div className="welcome">
+      <div className="welcome-page-container">
+        <Header />
+        <div className="welcome-header-box">
+          {userData && (
+            <>
+              <h2>ברוך הבא לאזור האישי שלך {userData.user.username}</h2>
+            </>
+          )}
         </div>
-    );
+
+        {/* מציגים את הטופס כולל הכפתור שכבר יש בו */}
+        <div style={{ marginTop: "30px" }}>
+          <StudentRequestForm />
+        </div>
+
+        <div style={{ marginTop: "40px" }}>
+          <StudentRequestsTable />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Student;
