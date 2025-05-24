9a6025cf5e7d095a390863144731bd1d7917d0b8
batelga <batelga@ac.sce.ac.il>
2025-05-24 22:26:05 +0300

    פתרון כל הקונפליקטים ואיחוד ענף main לתוך batel

--- frontend/src/header.jsx
+++ frontend/src/header.jsx
@@ -1,23 +1,5 @@
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
-import {logout} from './utils/services';
-const Header = ()=>{
 
-    const navigate= useNavigate();
-
-    const handleLogout = (e)=>{
-        e.preventDefault();
-        logout();
-        return navigate('/');
-        
-    }
-    return(<div className="header">
-        <div className="nav-conatiner">
-        <nav>
-            <div className="item"><a href="#" onClick={handleLogout} className="logout-link">התנתק</a>
-            </div>
-        </nav>
-=======
 import { logout } from './utils/services';
 
 const Header = () => {
@@ -36,7 +18,6 @@ const Header = () => {
                 <div className="item"><a href="#" onClick={handleLogout} className="logout-link">התנתק</a>
                 </div>
             </nav>
->>>>>>> main
         </div>
     </div>)
 }
